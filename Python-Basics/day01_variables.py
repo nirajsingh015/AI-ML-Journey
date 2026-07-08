623b5c47dd8cd1c0d5355bece52fc559a3828eb0
nirajsingh015 <nirajsingh01415@gmail.com>
2026-07-08 23:04:05 +0530

    Day-01 complted

---
+++ Python-Basics/day01_variables.py
@@ -0,0 +1,11 @@
+print("Hello AI/ML World!")
+
+name= "Niraj"
+age= 22
+is_student= True
+height=5.11
+
+print("Name:",name)
+print("Age:",age)
+print("Student:",is_student)
+print("Height:",height)
