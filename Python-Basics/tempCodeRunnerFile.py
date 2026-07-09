121539cfd093d6c880b5f583190d61155d007deb
nirajsingh015 <nirajsingh01415@gmail.com>
2026-07-09 23:54:00 +0530

    day02

--- Python-Basics/tempCodeRunnerFile.py
+++ Python-Basics/tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-print("Hello AI/ML World!")
-
-name= "Niraj"
-age= 22
-is_student= True
-height=5.11
-
-print("Name:",name)
-print("Age:",age)
-print("Student:",is_student)
-print("Height:",height)
+print(5)
