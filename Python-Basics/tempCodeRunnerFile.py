7240cdea04536ebea668e466cd63b8bc7c8865ac
nirajsingh015 <nirajsingh01415@gmail.com>
2026-07-30 23:03:45 +0530

    updated code

--- Python-Basics/tempCodeRunnerFile.py
+++ Python-Basics/tempCodeRunnerFile.py
@@ -1,6 +1,4 @@
-c=1.9
-print(type(c))
-d=8
-print(type(d))
-print(c+d)
-print(type(c+d))
+#so if we want to add then we need to use typecasting
+x=input("enter first number")
+y= input("enter second number")
+print(int(x)+int(y))
