ca902a5b074f3f76cc56ca13c57efb99b5732e08
nirajsingh015 <nirajsingh01415@gmail.com>
2026-07-30 23:06:12 +0530

    updated code

--- Python-Basics/day-09.py
+++ Python-Basics/day-09.py
@@ -1,2 +1,13 @@
 #taking input from user
 #we use input()
+a= input("enter your name")
+print("my name is : ",a)
+
+x=input("enter first number")
+y= input("enter second number")
+print(x+y)
+#here python will concatinate isntead of adding
+#so if we want to add then we need to use typecasting
+x=input("enter first number")
+y= input("enter second number")
+print(int(x)+int(y))
