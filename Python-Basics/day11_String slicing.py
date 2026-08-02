d5b22fef14fee98d68d0fa31638775e7f30913f4
nirajsingh015 <nirajsingh01415@gmail.com>
2026-08-02 23:01:48 +0530

    updated code

--- Python-Basics/day11_String slicing.py
+++ Python-Basics/day11_String slicing.py
@@ -11,7 +11,7 @@ print("mango is a", len1,'letter word.')
 # print(fruit[1:5])#including 1 but not 5
 # print(fruit[:5])
 # print(fruit[:])#IT AUTOMATICALLY FETCHES THE LENGTH
-# print(fruit[0:-3])#NEGATIVE SLICING HER - 3 IS INTERPERTED AS LEN OF FRUIT-3 = 2 WHICH IS 0 AND 1 INDEX
+# print(fruit[0:-3])#NEGATIVE SLICING HER - 3 IS INTERPRETED AS LEN OF FRUIT-3 = 2 WHICH IS 0 AND 1 INDEX
 print(fruit[-1:len(fruit)-3])
 print(fruit[-3:-1]) #output is ng 
 #this is because
