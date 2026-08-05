5d9e3ae8a3af3a0f038402d64f941216db01ae02
nirajsingh015 <nirajsingh01415@gmail.com>
2026-08-06 01:14:59 +0530

    day-14 completed

--- Python-Basics/day14_if else statements in pytohn.py
+++ Python-Basics/day14_if else statements in pytohn.py
@@ -46,3 +46,17 @@ else:
 # Number is positive
 # Enter the value of num: 0
 # Number is zero. 
+
+'''nested if else:
+we can use if, id-else, elif statements inside other if statements as well.'''
+num=18
+if(num<0):
+    print("Number is negative")
+elif(num>0):
+    if(num<=10):
+        print("Number is between 1-10. ")
+    elif(num>10 and num <=20):
+        print("Number is between 11-20 ")
+else:
+    print("Number is greater than 20.")
+# Number is between 11-20 (output) 
