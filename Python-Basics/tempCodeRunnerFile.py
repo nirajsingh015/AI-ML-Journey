1120c0684839cbd0e53a80ae2e9e43096c18ef81
nirajsingh015 <nirajsingh01415@gmail.com>
2026-08-13 00:22:53 +0530

    day-17 completed

--- Python-Basics/tempCodeRunnerFile.py
+++ Python-Basics/tempCodeRunnerFile.py
@@ -1,13 +1,3 @@
-x=int(input("Enter the value of x:"))
-# xis a variable to match.
-match x:
-    case 0:
-        print("x is zero")
-    case 4:
-        print("x is 4")
-    case _ if x!=80:
-        print(x)
-    case _ if x!=90:
-        print(x)
-    #case _:
-      #  print(x)
+
+for i in range(1, 12, 3):
+    print(i)
