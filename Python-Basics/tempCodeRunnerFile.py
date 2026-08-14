052f8345cd4106fabed613a3f36549df4f6949b1
nirajsingh015 <nirajsingh01415@gmail.com>
2026-08-15 00:45:50 +0530

    day19 completed

--- Python-Basics/tempCodeRunnerFile.py
+++ Python-Basics/tempCodeRunnerFile.py
@@ -1,7 +1,4 @@
-
-while True:
-    num=int(input("Enter your number"))
-    print("you entered:",num)
-
-    if num==0:
-        break
+for i in [2,3,4,5,6,7,8,0]:
+    if(i%2!=0):
+        continue
+    print(i)
