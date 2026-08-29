3dc196dda7fdc1f6178d2a85d8111d387a4f161d
nirajsingh015 <nirajsingh01415@gmail.com>
2026-08-29 23:29:43 +0530

    Day 20 Completed

--- Python-Basics/tempCodeRunnerFile.py
+++ Python-Basics/tempCodeRunnerFile.py
@@ -1,4 +1,23 @@
-for i in [2,3,4,5,6,7,8,0]:
-    if(i%2!=0):
-        continue
-    print(i)
+def calculate_gemean(a,b): #snakecase
+  mean= (a*b)/(a+b)
+  print(mean) 
+
+
+def isGreater(a,b):
+  if(a>b):
+    print("First is greater")
+  else:
+    print("second number is smaller")
+
+def isLesser(a,b):
+  pass #pass is used to continue the program if the fucntions is missing and i decided to write that later.
+
+
+a=9
+b=8
+c=8
+d=7
+#calculateGemean(a,b)
+calculate_gemean(a,b)
+isGreater(a, b)
+isGreater(c,d)
